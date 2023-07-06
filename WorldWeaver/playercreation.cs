79e9c5b87ab567a65afba73b3a0e631a0f444c50
JAC <j.aguayo8@gmail.com>
2023-07-06 15:36:16 -0800

    Stats save

--- WorldWeaver/playercreation.cs
+++ WorldWeaver/playercreation.cs
@@ -33,6 +33,7 @@ namespace WorldWeaver
             connection.Open();
 
             string query = "INSERT INTO player (player_name, race, level, class) VALUES ('" + textBox1.Text + "', '" + textBox2.Text + "', " + textBox3.Text + ", '" + textBox4.Text + "')";
+            query = "INSERT INTO stats (hit_points, strength, dexterity, constitution, intelligence, wisdom, charisma, proficiency_bonus, walking_speed, initiative, armor_class) VALUES ('" + textBox5.Text + "', '" + textBox6.Text + "', '" + textBox7.Text + "', '" + textBox8.Text + "', '" + textBox9.Text +"', '" + textBox10.Text + "', '" + textBox11.Text + "', '" + textBox12.Text + "', '" + textBox13.Text + "', '" + textBox14.Text + "', '" + textBox15.Text + "')";
             SqlCommand command = new SqlCommand(query, connection);
             command.ExecuteNonQuery();
             connection.Close();
