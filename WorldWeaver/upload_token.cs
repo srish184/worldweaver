3e033a8b969772d51c65cac46858e35bba89d484
Mike Fahmie <fahmiemike@gmail.com>
2023-07-07 17:12:46 -0400

    small changes to token_upload after self testing. Ready for QA to test

--- WorldWeaver/upload_token.cs
+++ WorldWeaver/upload_token.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Configuration;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WorldWeaver
 {
@@ -48,17 +48,34 @@ namespace WorldWeaver
                 // Get the selected file path
                 selectedFilePath = openFileDialog.FileName;
 
-                // Load the selected image into the PictureBox
-                Image selectedImage = Image.FromFile(selectedFilePath);
+                try
+                {
+                    // Check if the file extension is valid
+                    string extension = Path.GetExtension(selectedFilePath).ToLower();
+                    if (extension == ".jpeg" || extension == ".jpg" || extension == ".png")
+                    {
+                        // Load the selected image into the PictureBox
+                        Image selectedImage = Image.FromFile(selectedFilePath);
 
-                // Resize the image to fit within the PictureBox
-                img_tokenPreview.SizeMode = PictureBoxSizeMode.StretchImage;
-                img_tokenPreview.Image = selectedImage;
+                        // Resize the image to fit within the PictureBox
+                        img_tokenPreview.SizeMode = PictureBoxSizeMode.StretchImage;
+                        img_tokenPreview.Image = selectedImage;
 
-                // Enable the submit button
-                btn_submit.Enabled = true;
+                        // Enable the submit button
+                        btn_submit.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid image format. Only .jpeg, .jpg, and .png formats are supported.", "Invalid Format", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
+
 
 
         private void btn_submit_Click(object sender, EventArgs e)
@@ -71,33 +88,52 @@ namespace WorldWeaver
 
             try
             {
-                // Create the token_images subfolder if it doesn't exist
-                string subfolderPath = Path.GetFullPath(Path.Combine(Application.StartupPath, @"..\token_images"));
-                if (!Directory.Exists(subfolderPath))
-                {
-                    Directory.CreateDirectory(subfolderPath);
-                }
-
-                // Get the file name from the file path
-                string fileName = Path.GetFileName(selectedFilePath);
-
-                // Build the destination path for the image
-                string destinationPath = Path.Combine(subfolderPath, fileName);
-
-                // Copy the image file to the destination path
-                File.Copy(selectedFilePath, destinationPath, true);
-
                 // Save the token information to the database
                 string tokenName = txt_tokenName.Text;
+                string destinationPath = SaveTokenImage();
+
+                // Insert token data into the database
                 SaveTokenToDatabase(tokenName, destinationPath);
 
-                // Show a success message
-                MessageBox.Show("Token saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Show a success message and ask if the user wants to add another token
+                var result = MessageBox.Show("Token saved successfully! Want to add another token now?", "Success", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Clear the form and allow the user to add another token
+                    ClearForm();
+                }
+                else if (result == DialogResult.No)
+                {
+                    // Return to the main menu
+                    ReturnToMainMenu();
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error saving token: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private string SaveTokenImage()
+        {
+            // Create the token_images subfolder if it doesn't exist
+            string subfolderPath = Path.GetFullPath(Path.Combine(Application.StartupPath, @"..\token_images"));
+            if (!Directory.Exists(subfolderPath))
+            {
+                Directory.CreateDirectory(subfolderPath);
+            }
+
+            // Get the file name from the file path
+            string fileName = Path.GetFileName(selectedFilePath);
+
+            // Build the destination path for the image
+            string destinationPath = Path.Combine(subfolderPath, fileName);
+
+            // Copy the image file to the destination path
+            File.Copy(selectedFilePath, destinationPath, true);
+
+            return destinationPath;
         }
 
         private void SaveTokenToDatabase(string tokenName, string destinationPath)
@@ -117,6 +153,25 @@ namespace WorldWeaver
                 }
             }
         }
+
+        private void ClearForm()
+        {
+            txt_tokenName.Text = string.Empty;
+            img_tokenPreview.Image = null;
+            btn_submit.Enabled = false;
+            selectedFilePath = string.Empty;
+            lbl_tokenFilePicker.Visible = false;
+            btn_tokenFilePicker.Visible = false;
+        }
+
+        private void ReturnToMainMenu()
+        {
+            // Hide the current form (upload_token)
+            this.Hide();
+
+            // Show the main_menu form
+            main_menu mainMenuForm = new main_menu();
+            mainMenuForm.Show();
+        }
     }
 }
-
